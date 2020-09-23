27e5dd60e1190389b67eea4a6407d3c51c65f397
Megasteel32 <megasteel32@gmail.com>
2020-09-23 11:48:10 -0500

    Initial commit

---
+++ program1.py
@@ -0,0 +1,8 @@
+# By submitting this assignment, I agree to the following:
+#  "Aggies do not lie, cheat, or steal, or tolerate those who do"
+#  "I have not given or received any unauthorized aid on this assignment"
+#
+# Name:        Luca Maddaleni, 330001030
+# Section:     273
+# Assignment:  Stress Thingy
+# Date:        9/14/2020
