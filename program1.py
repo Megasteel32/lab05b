6c0cfe62cde4e6646fa45050022e53e91377e2a6
Megasteel32 <megasteel32@gmail.com>
2020-09-23 12:01:14 -0500

    poop

--- program1.py
+++ program1.py
@@ -4,5 +4,30 @@
 #
 # Name:        Luca Maddaleni, 330001030
 # Section:     273
-# Assignment:  Stress Thingy
-# Date:        9/14/2020
+# Assignment:  Averaging Measurements
+# Date:        9/23/2020
+
+# Defining variables
+stored_amt = 0
+user_amt = 0
+running_avg = 0
+running_min = 0
+running_max = 0
+counter = 0
+
+# Asking for user input, the main loop
+while user_amt >= 0:
+    user_amt = int(input("Enter in data! "))
+    if user_amt >= 0:
+        counter += 1
+        stored_amt += user_amt
+        if user_amt > running_max:
+            running_max = user_amt
+        if user_amt < running_min:
+            running_min = user_amt
+        running_avg = (running_avg + user_amt) / counter
+
+# Print final values
+print("Running max:",running_max)
+print("Running min:",running_min)
+print("Running avg:",running_avg)
